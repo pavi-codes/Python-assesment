e5e058105159a8f6b7f172856f49faa9f0d912a6
Pavithra G <75522541+pavi-codes@users.noreply.github.com>
2021-09-06 17:20:58 +0530

    Update text.py

--- text.py
+++ text.py
@@ -3,3 +3,14 @@ GIT COMMANDS ARE:
   git add
   git commit -m "code to know later"
   git push
+  git init
+  git log
+  git reset
+  git branch
+  git merge <branch-name>
+  git clone <link>
+  git pull
+  git add .
+  hit checkout -b <branchname>
+  
+  
